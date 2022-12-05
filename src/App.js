4dc331ed7e57139d4154db3ce6a9b869c623b874
EraNova94 <103873419+EraNova94@users.noreply.github.com>
2022-12-05 20:04:30 +0600

    quiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
+
+const App = () => {
+  const [step, setStep] = useState(0);
+  const question = questions[step];
+  const [correct, setCorrect] = useState(0);
+
+  const persentage = Math.round((step / questions.length) * 100);
+  const onclickVariant = index => {
+    console.log(step, index);
+    setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {step !== questions.length ? (
+        <div className="quiz">
+          <h2>Quiz</h2>
+          <br />
+          <div className="progress">
+            <div
+              style={{ width: `${persentage}%` }}
+              className="progress__inner"></div>
+          </div>
+          <h1>{question.title}</h1>
+          <ul>
+            {question.variants.map((text, index) => (
+              <li key={text} onClick={() => onclickVariant(index)}>
+                {text}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div className="result">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
+            alt="congrat-img"
+          />
+          <h2>
+            Вы отгадали {correct} ответа из {questions.length}
+          </h2>
+          <a href="/">
+            <button>Попробовать снова</button>
+          </a>
+        </div>
+      )}
+    </>
   );
-}
-
+};
 export default App;
